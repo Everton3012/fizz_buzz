7d34772899de8f73e66b3135ba8ef1cc5817c19c
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-30 16:49:52 -0300

    tratamento de erros

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -6,12 +6,26 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-    def handle_file_read({:ok, result}) do
-      result
-      |> String.split(",")
-      |> Enum.map(& String.to_integer/1)
+    defp handle_file_read({:ok, result}) do
+      result =
+        result
+        |> String.split(",")
+        |> Enum.map(&convert_end_evalute_number/1)
+      {:ok, result}
     end
-    def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+    defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+    defp convert_end_evalute_number(elem) do
+      elem
+      |> String.to_integer()
+      |> evaluate_number()
+    end
+
+    defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+    defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+    defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+    defp evaluate_number(number), do: number
+
 
     #case File.read(file_name) do
     #  {:ok, result} -> result
