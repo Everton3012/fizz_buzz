06796a8bb8b508f02ef1cd9929650d004340d997
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-30 16:10:12 -0300

    transformar string em numero e separar por virgula

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -6,8 +6,12 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-    def handle_file_read({:ok, result}), do: result
-    def handle_file_read({:error, reason}), do: reason
+    def handle_file_read({:ok, result}) do
+      result
+      |> String.split(",")
+      |> Enum.map(& String.to_integer/1)
+    end
+    def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 
     #case File.read(file_name) do
     #  {:ok, result} -> result
