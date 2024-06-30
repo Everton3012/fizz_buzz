b9d182f8c65b0631b9dee8ad11915e61531edb01
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-30 16:01:00 -0300

    leitura de arquivo txt

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,12 +1,17 @@
 defmodule FizzBuzz do
 
   def build(file_name) do
-
-    case File.read(file_name) do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-     end
-
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
+
+    def handle_file_read({:ok, result}), do: result
+    def handle_file_read({:error, reason}), do: reason
+
+    #case File.read(file_name) do
+    #  {:ok, result} -> result
+    #  {:error, reason} -> reason
+    # end
 
 end
